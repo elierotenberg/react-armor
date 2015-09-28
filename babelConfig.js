174b50d3292cd4cf43c392cc63f4349aa78b1344
Elie Rotenberg <elie@rotenberg.io>
2015-09-28 02:12:43 +0200

    Enable babel loose mode excep es6.spread

--- babelConfig.js
+++ babelConfig.js
@@ -6,4 +6,12 @@ module.exports = {
     'es7.decorators',
     'es7.objectRestSpread',
   ],
+  loose: [
+    'es6.classes',
+    'es6.destructuring',
+    'es6.properties.computed',
+    'es6.modules',
+    'es6.forOf',
+    'es6.templateLiterals',
+  ],
 };
