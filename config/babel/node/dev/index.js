52ff314c1c38e841d3178af9759629f1c75b65be
Elie Rotenberg <elie@rotenberg.io>
2015-12-15 00:14:04 +0100

    Revert "Fixed some babel issues"
    This reverts commit 156de587964d6ec8ee039166cd5d281a48da006f.

--- config/babel/node/dev/index.js
+++ config/babel/node/dev/index.js
@@ -20,7 +20,6 @@ module.exports = {
     'babel-plugin-transform-exponentiation-operator',
     'babel-plugin-transform-object-rest-spread',
     'babel-plugin-transform-flow-strip-types',
-    'babel-plugin-transform-react-jsx',
     'babel-plugin-transform-eval',
     // 'babel-plugin-transform-jscript',
     'babel-plugin-transform-object-assign',
